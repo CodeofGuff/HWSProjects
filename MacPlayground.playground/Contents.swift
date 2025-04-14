60d5cb28d9dae0d23921545edc79c012d3ddf662
guFF <161796631+CodeofGuff@users.noreply.github.com>
2025-04-14 01:44:40 -0700

    k

--- MacPlayground.playground/Contents.swift
+++ MacPlayground.playground/Contents.swift
@@ -1,10 +1,20 @@
 import Cocoa
 
-var greeting = "Hello, playground."
-
-
-if greeting == "Hello, playground" {
-	print("Correct!")
-} else {
-	print("Oops")
+struct Employee {
+	let name: String
+	var vacayAllocated = 14
+	var vacayTaken = 0
+	var vacayRemaining: Int {
+		get {
+			vacayAllocated - vacayTaken
+		}
+		set {
+			vacayAllocated = vacayTaken + newValue
+		}
+	}
 }
+
+var archer = Employee(name: "Sterling Archer", vacayAllocated: 10)
+archer.vacayTaken += 4
+archer.vacayRemaining = 5
+print(archer.vacayAllocated)
