ea2a40caf8fb71d6059dc274302d56b330b7e789
guFF <161796631+CodeofGuff@users.noreply.github.com>
2025-06-07 14:18:31 -0700

    k

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,13 +7,30 @@
 
 import SwiftUI
 
+struct GridStack<Content: View>: View {
+	let columns: Int
+	let rows: Int
+	@ViewBuilder let content: (Int, Int) -> Content
+
+	var body: some View {
+		VStack {
+			ForEach(0..<rows, id: \.self) { row in
+				HStack {
+					ForEach(0..<columns, id: \.self) { column in
+						content(row, column)
+					}
+				}
+			}
+		}
+	}
+}
+
 struct ContentView: View {
 	var body: some View {
-		Button("Hey there") {
-			print("Hi!")
+		GridStack(columns: 4, rows: 4) { col, row in
+			Image(systemName: "\(row * 4 + col).circle")
+			Text("R\(row) C\(col)")
 		}
-		.frame(width: 200, height: 50)
-		.background(.red)
 	}
 }
 
